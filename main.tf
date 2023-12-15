12df707ecef65f8ceca5814055e484b85f16c7a1
Sayana Boz <sayanaboz@gmail.com>
2023-12-15 23:13:01 +0000

    First Playbook

---
+++ main.tf
@@ -0,0 +1,47 @@
+variable "zone" {
+  default = "us-central1-f"
+}
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+
+
+  project = "final-408221"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  tags         = ["dev", "web"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
